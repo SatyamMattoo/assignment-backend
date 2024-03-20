f62c3b71afed110dee1430a67b1cfe0c17f3d800
SatyamMattoo <satyammattoo2003@gmail.com>
2024-03-20 19:48:04 +0530

    fixing cors bug

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 
-import submissionRouter from "./routes/submissionRouter.js"
+import submissionRouter from "./routes/submissionRouter.js";
 
 export const app = express();
 
 const corsOptions = {
-  //   origin: process.env.FRONTEND_URL,
-  origin: "*",
+  origin: process.env.FRONTEND_URL,
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
 };
@@ -18,7 +17,7 @@ app.use(cors(corsOptions));
 //Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/api/v1/submission",  submissionRouter);
+app.use("/api/v1/submission", submissionRouter);
 
 app.get("/", (req, res) => {
   res.send("hi");
